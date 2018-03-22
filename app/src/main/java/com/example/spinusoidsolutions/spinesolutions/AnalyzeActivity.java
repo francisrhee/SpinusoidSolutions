ac3b244831960e212a53afd9a2d78a43ea7b1dce
Noah Kunej <nakunej@uwaterloo.ca>
2018-03-22 01:25:41 -0300

    analytics is a go, able to make graphs by reading json file

--- app/src/main/java/com/example/spinusoidsolutions/spinesolutions/AnalyzeActivity.java
+++ app/src/main/java/com/example/spinusoidsolutions/spinesolutions/AnalyzeActivity.java
@@ -29,10 +29,10 @@ import java.io.Reader;
         import java.nio.file.Paths;
         import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+        import java.time.LocalDate;
+        import java.time.LocalDateTime;
+        import java.util.Date;
         import java.util.List;
-
-
 
 //Following imports are necessary for JSON parsing
 
@@ -42,71 +42,54 @@ public class AnalyzeActivity extends AppCompatActivity {
 //    @SuppressLint("SimpleDateFormat")
     @Override
 
-
-
-
     protected void onCreate(Bundle savedInstanceState) {
-
-
-
-
-// try {
-//            new ReadJsonList().run();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        try{
-//            InputStream is = getAssets().open("SpinusoidData.json");
-//            int size = is.available();
-//            byte[] buffer = new byte[size];
-//            is.read();
-//            is.close();
-//
-//            String json = new String(buffer,"UTF-8");
-//
-//
-//            Moshi moshi = new Moshi.Builder().build();
-//
-//            Type spineDataType = Types.newParameterizedType(List.class, SpineData.class);
-//            JsonAdapter<List<SpineData>> jsonAdapter = moshi.adapter(spineDataType);
-//
-//            List<SpineData> dataArray = jsonAdapter.fromJson(json);
-//            System.out.println(dataArray);
-//        }catch (Exception e) {
-//            e.printStackTrace();
-//        }
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_analyze);
+        SimpleDateFormat neatDateFormatter= new SimpleDateFormat("MM/dd/yyyy");
+        Date beginningFilterDate = new Date();
+        Date endingFilterDate = new Date();
 
         try {
-            run();
+            beginningFilterDate = neatDateFormatter.parse("03/03/2018");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        try {
+            endingFilterDate = neatDateFormatter.parse("04/01/2018");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            createGraphs(beginningFilterDate, endingFilterDate);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        String [] stringDates = new String[5];
-        stringDates[0] = "03/01/2018";
-        stringDates[1] = "03/02/2018";
-        stringDates[2] = "03/03/2018";
-        stringDates[3] = "03/04/2018";
-        stringDates[4] = "03/05/2018";
+    }
 
-        int[] data = new int[5];
-        data[0] = 5;
-        data[1] = 4;
-        data[2] = 5;
-        data[3] = 3;
-        data[4] = 8;
+    public void createGraphs(Date beginningFilterDate, Date endingFilterDate) throws Exception {
+
+        String fileName = "SpinusoidData.json";
+//      reading json file, converting to string
+        String json = loadJSONFromAsset(this);
+        Moshi moshi = new Moshi.Builder().build();
+
+        Type spineDataType = Types.newParameterizedType(List.class, SpineData.class);
+        JsonAdapter<List<SpineData>> jsonAdapter = moshi.adapter(spineDataType);
+
+//      creating list of objects
+        List<SpineData> dataArray = jsonAdapter.fromJson(json);
 
         SimpleDateFormat dateFormatter=new SimpleDateFormat("MM/dd/yyyy");
         GraphView graph = (GraphView) findViewById(R.id.graph);
         LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>();
 //        Date currentDate = new Date();
-        Date [] dates = new Date[5];
-        for(int i =0; i < stringDates.length; i++)
+        Date [] dates = new Date[dataArray.size()];
+        for(int i =0; i < dataArray.size(); i++)
         {
             try {
-                dates[i] = dateFormatter.parse(stringDates[i]);
+                dates[i] = dateFormatter.parse(dataArray.get(i).date);
             }
             catch (ParseException e)
             {
@@ -114,151 +97,37 @@ public class AnalyzeActivity extends AppCompatActivity {
             }
         }
 
-        for(int i =0; i < data.length; i++)
+        for(int i =0; i < dataArray.size(); i++)
         {
-            series.appendData(new DataPoint(dates[i], data[i]), true, data.length);
+            if(dates[i].after(beginningFilterDate) && dates[i].before(endingFilterDate))
+            series.appendData(new DataPoint(dates[i], dataArray.get(i).difference), true, dataArray.size());
         }
         graph.addSeries(series);
         // set date label formatter
         graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(this));
         graph.getGridLabelRenderer().setNumHorizontalLabels(dates.length); // only 4 because of the space
 
-// set manual x bounds to have nice steps
+        // set manual x bounds to have nice steps
         graph.getViewport().setMinX(dates[0].getTime());
         graph.getViewport().setMaxX(dates[dates.length-1].getTime());
         graph.getViewport().setXAxisBoundsManual(true);
-
-// as we use dates as labels, the human rounding to nice readable numbers
-// is not necessary
         graph.getGridLabelRenderer().setHumanRounding(true);
-    }
-
-    public void run() throws Exception {
-
-        String fileName = "SpinusoidData.json";
-
-        String json = loadJSONFromAsset(this);
-        Moshi moshi = new Moshi.Builder().build();
-
-        Type spineDataType = Types.newParameterizedType(List.class, SpineData.class);
-        JsonAdapter<List<SpineData>> jsonAdapter = moshi.adapter(spineDataType);
-
-        List<SpineData> dataArray = jsonAdapter.fromJson(json);
-        System.out.println(dataArray);
+//
     }
     public String loadJSONFromAsset(Context context) {
         String json = null;
         try {
             InputStream is = context.getAssets().open("SpinusoidData.json");
-
             int size = is.available();
-
             byte[] buffer = new byte[size];
-
             is.read(buffer);
-
             is.close();
-
             json = new String(buffer, "UTF-8");
-
 
         } catch (IOException ex) {
             ex.printStackTrace();
             return null;
         }
         return json;
-
     }
-
 }
-//    public static void main(String[] args) throws IOException {
-//        try(Reader reader = new InputStreamReader(JsonToJava.class.getResourceAsStream("/Server1.json"), "UTF-8")){
-//            Gson gson = new GsonBuilder().create();
-//            Person p = gson.fromJson(reader, Person.class);
-//            System.out.println(p);
-//        }
-//    }
-
-
-//    public final class ReadJsonList {
-//        public void run() throws Exception {
-////
-//            InputStream is = getAssets().open("SpinusoidData.json");
-//            int size = is.available();
-//            byte[] buffer = new byte[size];
-//            is.read();
-//            is.close();
-//
-//            String json = new String(buffer,"UTF-8");
-//
-//
-//            Moshi moshi = new Moshi.Builder().build();
-//
-//            Type spineDataType = Types.newParameterizedType(List.class, SpineData.class);
-//            JsonAdapter<List<SpineData>> jsonAdapter = moshi.adapter(spineDataType);
-//
-//            List<SpineData> dataArray = jsonAdapter.fromJson(json);
-//            System.out.println(dataArray);
-//            }
-//        }
-
-
-//    public void get_json()
-//    {
-//        String json;
-//        try
-//        {
-//            InputStream is = getAssets().open("SpinusoidData.json");
-//            int size = is.available();
-//            byte[] buffer = new byte[size];
-//            is.read();
-//            is.close();
-//
-//            json = new String(buffer,"UTF-8");
-//            JSONArray jsonArray = new JSONArray(json);
-//
-//            for(int i =0; i < jsonArray.length();i++){
-//                JSONObject obj = jsonArray.getJSONObject(i);
-//                if(obj.getString(""))
-//            }
-//
-//        }catch (IOException e)
-//        {
-//            e.printStackTrace();
-//        }
-
-//    public JSONObject parseJSONData() {
-//        String JSONString = null;
-//        JSONObject JSONObject = null;
-//        try {
-//
-//            //open the inputStream to the file
-//            InputStream inputStream = getAssets().open("SpinusoidData.json");
-//
-//            int sizeOfJSONFile = inputStream.available();
-//
-//            //array that will store all the data
-//            byte[] bytes = new byte[sizeOfJSONFile];
-//
-//            //reading data into the array from the file
-//            inputStream.read(bytes);
-//
-//            //close the input stream
-//            inputStream.close();
-//
-//            JSONString = new String(bytes, "UTF-8");
-//            JSONObject = new JSONObject(JSONString);
-//
-//        } catch (IOException ex) {
-//            ex.printStackTrace();
-//            return null;
-//        }
-//        catch (JSONException x) {
-//            x.printStackTrace();
-//            return null;
-//        }
-//        return JSONObject;
-//    }
-//}
-
-
---
+++ app/src/main/java/com/example/spinusoidsolutions/spinesolutions/allOfPatientData.java
@@ -0,0 +1,12 @@
+package com.example.spinusoidsolutions.spinesolutions;
+
+import java.util.List;
+
+/**
+ * Created by Noah on 2018-03-22.
+ */
+
+public class allOfPatientData {
+    List patientDataArray;
+
+}
